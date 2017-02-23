a985ccd073af072a4ea88dafac856107695f2abf
Evan Bishop and ryanjonesirl@gmail.com <ryanjonesirl@gmail.com@>
2017-02-23 09:03:06 -0800

    add a forloop to cycle through 1-5 and print out the proper roman numeral

--- js/scripts.js
+++ js/scripts.js
@@ -14,3 +14,21 @@ if(number === 1) {
 } else if (number === 5) {
   console.log("V");
 }
+
+// using a for loop to do 1-5
+var number = 5;
+var string = "";
+var finalString = "";
+var sliced = "";
+
+for(i=0; i<=number; i++) {
+	if(i >= 0 && i < 3) {
+  	finalString = finalString + string + "I";
+    console.log("Final String, in 0-3 range: " + finalString);
+  } else if (i === 4) {
+  	sliced = finalString.slice(0, i-3); // III = I (i-3)
+  	console.log("Sliced, at 4: " + sliced);
+  } else if (i === 5) {
+  	console.log("V");
+  }
+}
