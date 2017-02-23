123ad1622b8ef86b0901c5dbf59264d18db55f30
Evan Bishop and ryanjonesirl@gmail.com <ryanjonesirl@gmail.com@>
2017-02-23 08:30:20 -0800

    add directories, and logic for first test case (1-5 converting to roman numerals)

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+/**** First Test Case ****/
+// we are getting 1-5 to console log
+
+var number = 4;
+
+if(number === 1) {
+	console.log("I");
+} else if (number === 2) {
+	console.log("II");
+} else if (number === 3) {
+	console.log("III");
+} else if (number === 4) {
+	console.log("IV");
+} else if (number === 5) {
+  console.log("V");
+}
