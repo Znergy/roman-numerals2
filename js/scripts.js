7e5d2b7e82d88b8e652f600b6c86ba7169788e31
Evan Bishop and ryanjonesirl@gmail.com <evan@>
2017-02-23 11:32:32 -0800

    several sttempts at roman numerals

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,46 @@
 /**** First Test Case ****/
 // we are getting 1-5 to console log
-
-var number = 4;
-
-if(number === 1) {
-	console.log("I");
-} else if (number === 2) {
-	console.log("II");
-} else if (number === 3) {
-	console.log("III");
-} else if (number === 4) {
-	console.log("IV");
-} else if (number === 5) {
-  console.log("V");
-}
+// 
+// var number = 4;
+//
+// if(number === 1) {
+// 	console.log("I");
+// } else if (number === 2) {
+// 	console.log("II");
+// } else if (number === 3) {
+// 	console.log("III");
+// } else if (number === 4) {
+// 	console.log("IV");
+// } else if (number === 5) {
+//   console.log("V");
+// }
 
 // using a for loop to do 1-5
-var number = 5;
-var string = "";
-var finalString = "";
-var sliced = "";
+// var number = 5;
+// var string = "";
+// var finalString = "";
+// var sliced = "";
+//
+// for(i=0; i<=number; i++) {
+// 	if(i >= 0 && i < 3) {
+//   	finalString = finalString + string + "I";
+//     console.log("Final String, in 0-3 range: " + finalString);
+//   } else if (i === 4) {
+//   	sliced = finalString.slice(0, i-3); // III = I (i-3)
+//   	console.log("Sliced, at 4: " + sliced);
+//   } else if (i === 5) {
+//   	console.log("V");
+//   }
+// }
 
-for(i=0; i<=number; i++) {
-	if(i >= 0 && i < 3) {
-  	finalString = finalString + string + "I";
-    console.log("Final String, in 0-3 range: " + finalString);
-  } else if (i === 4) {
-  	sliced = finalString.slice(0, i-3); // III = I (i-3)
-  	console.log("Sliced, at 4: " + sliced);
-  } else if (i === 5) {
-  	console.log("V");
-  }
-}
+// SECOND ATTMEPT AT ROMAN NUMERALS
+var I = 1;
+var V = 5;
+var X = 10;
+var L = 50;
+var C = 100;
+var D = 500;
+var M = 1,000;
+
+numArray = [1, 5, 10, 50, 100, 500, 1000]
+letterArray = [I, V, X, L, C, D, M]
